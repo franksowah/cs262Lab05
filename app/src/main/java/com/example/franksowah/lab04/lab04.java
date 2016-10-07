9b3390d62fe4c7ed5c14092a8673837679f61116
franksowah <efb4@students.calvin.edu>
2016-10-07 15:18:51 -0400

    cs 262 lab05

--- app/src/main/java/com/example/franksowah/lab04/lab04.java
+++ app/src/main/java/com/example/franksowah/lab04/lab04.java
@@ -9,29 +9,56 @@
 package com.example.franksowah.lab04;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class lab04 extends AppCompatActivity {
 
-    Menu mainMenu = null;
+    private SharedPreferences prefs;
+    private boolean preference = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lab04);
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        showPreferences();
 
+    }
+
+    private void showPreferences() {
+        TextView preferenceTextView = (TextView) findViewById(R.id.preferenceTextView);
+        preferenceTextView.setText("Preference: " + prefs.getBoolean("preference", false));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_menu_lab04, menu);
-        mainMenu = menu;
         return true;
+    }
+
+    @Override
+    public void onPause() {
+        //save th instance variables
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean("preference", preference);
+        editor.commit();
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        preference = prefs.getBoolean("preference", false);
+        showPreferences();
     }
 
     @Override
